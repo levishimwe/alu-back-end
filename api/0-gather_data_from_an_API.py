aa9f527331277245a0724313d26f33550423c303
Levis Ishimwe <i.levis@alustudent.com>
2024-10-21 15:19:59 +0200

    fix some errors

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,7 +10,7 @@ def get_employee_info(employee_id):
     """
     Get employee information by employee ID
     """
-    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/'
     response = requests.get(url)
     return response.json()
 
@@ -30,21 +30,23 @@ def main(employee_id):
     """
     employee = get_employee_info(employee_id)
     employee_name = employee.get("name")
+
     emp_todos = get_employee_todos(employee_id)
-    total_tasks = len(emp_todos)
-    completed_tasks = sum(1 for todo in emp_todos if todo.get("completed"))
-    
-    print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, completed_tasks, total_tasks))
-    
-    # Print completed tasks with exact formatting
-    for todo in emp_todos:
-        if todo.get("completed"):
-            print("\t {}".format(todo.get('title')))
+    tasks = {todo.get("title"): todo.get("completed") for todo in emp_todos}
+
+    total_tasks = len(tasks)
+    completed_tasks = [completed for completed in tasks.values() if completed]
+    completed_tasks_count = len(completed_tasks)
+
+    print(f"Employee {employee_name} is done with tasks"
+          f"({completed_tasks_count}/{total_tasks}):")
+    for title, completed in tasks.items():
+        if completed:
+            print(f"\t {title}")
 
 
 if __name__ == "__main__":
     if len(argv) > 1:
-        main(int(argv[1]))
+        main(argv[1])
     else:
         print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
